cc894bf3543427e36c9ba6533110bbfb3029d1f9
Willy Tarreau <w@1wt.eu>
2017-09-18 01:15:37 +0200

    code: alarm: start to implement an alarm clock
    For now it retrieves the time over SNTP, displays it every time it
    switches to a new minute. It supports light fading in and out with
    the left button only for now, and doesn't yet support full alarms.

---
+++ code/alarm.lua
@@ -0,0 +1,151 @@
+-- alarm clock application
+
+-- debounced button states
+btn1_state=1
+btn2_state=1
+
+-- light states
+LS_IDLE=0
+LS_FULL_START=1
+LS_FULL=2
+LS_FULL_STOP=3
+LS_HALF_START=4
+LS_HALF=5
+LS_HALF_STOP=6
+
+-- transition delays in milliseconds
+LIGHT_FULL_TIME=30000
+LIGHT_STAY_TIME=600000
+LIGHT_HALF_TIME=5000
+LIGHT_STOP_TIME=10000
+
+-- light state, last change date, last brightness
+light_state=LS_IDLE
+light_change=0
+light_bright=0
+force_refresh=nil
+
+-- current time
+tyy,tmm,tdd,th,tm,ts,twd=0,0,0,0,0,0,0
+
+-- low level button handlers
+function btn1_trig()
+  local lev=debounce(brd_btn1)
+  if btn1_state ~= lev then
+    btn1_state = lev
+    btn1_cb()
+  end
+end
+
+function btn2_trig()
+  local lev=debounce(brd_btn2)
+  if btn2_state ~= lev then
+    btn2_state = lev
+    btn2_cb()
+  end
+end
+
+-- return the time spent in the current state in ms except idle (always 0)
+function light_duration()
+  local now=tmr.now()
+  if light_state == LS_IDLE then return 0 end
+  now=now-light_change
+  if now < 0 then now=now+2147483648 end
+  return now / 1000
+end
+
+function light_set_state(state)
+  light_change=tmr.now()
+  light_state=state
+end
+
+-- return the new brightness based on the time spent in the state (0..1023) and
+-- adjust transient states.
+function light_new_brightness()
+  local dur=light_duration()
+
+  if light_state == LS_FULL_START then
+    if dur < LIGHT_FULL_TIME then return light_duration() * 1023 / LIGHT_FULL_TIME end
+    light_set_state(LS_FULL)
+  end
+
+  if light_state == LS_HALF_START then
+    if dur < LIGHT_HALF_TIME then return light_duration() * 512 / LIGHT_HALF_TIME end
+    light_set_state(LS_HALF)
+  end
+
+  if light_state == LS_FULL then
+     if dur < LIGHT_STAY_TIME then return 1023 end
+     light_set_state(LS_FULL_STOP)
+  end
+
+  if light_state == LS_HALF then
+     if dur < LIGHT_STAY_TIME then return 512 end
+     light_set_state(LS_HALF_STOP)
+  end
+
+  if light_state == LS_FULL_STOP then
+    if dur < LIGHT_FULL_TIME then return 1023 - light_duration() * 1023 / LIGHT_FULL_TIME end
+     light_set_state(LS_IDLE)
+  end
+
+  if light_state == LS_HALF_STOP then
+    if dur < LIGHT_HALF_TIME then return 512 - light_duration() * 512 / LIGHT_HALF_TIME end
+     light_set_state(LS_IDLE)
+  end
+
+  return 0
+end
+
+
+-- set light's pwm from 0 to 1023
+function light_pwm(val)
+  pwm.setduty(brd_pwm, val)
+end
+
+-- manages screen and brightness
+function refresh()
+  local ratio=light_new_brightness()
+  local yy,mm,dd,h,m,s,wd=time_get_now()
+
+  if m ~= nil and (m ~= tm or force_refresh) then
+    force_refresh=nil
+    tyy,tmm,tdd,th,tm,ts,twd=yy,mm,dd,h,m,s,wd
+    disp_lines(string.format("%04d-%02d-%02d (%d)",yy,mm,dd,wd),
+               string.format("%02d:%02d:%02d",h,m,s),
+	       string.format("state=%d ratio=%d",light_state, ratio))
+  end
+
+  if ratio ~= light_bright then
+    light_bright=ratio
+    light_pwm(ratio)
+  end
+end
+
+-- high level button functions, debounced
+function btn1_cb()
+  print("btn1: ",btn1_state," st: ",light_state)
+  if btn1_state == 1 then return end
+  --if light_state == LS_IDLE then light_set_state(LS_FULL_START) end
+  if light_state == LS_IDLE then
+    light_set_state(LS_FULL_START)
+  elseif light_state < 6 then
+    light_set_state(light_state+1)
+  else
+    light_set_state(LS_IDLE)
+  end
+  force_refresh=1
+  refresh()
+end
+
+function btn2_cb()
+  print("btn2: ",btn2_state)
+end
+
+-- entry point
+
+gpio.trig(brd_btn1,"both",btn1_trig)
+gpio.trig(brd_btn2,"both",btn2_trig)
+pwm.setup(brd_pwm,500,0)
+pwm.start(brd_pwm)
+tmr.alarm(1,100,tmr.ALARM_AUTO,refresh)
