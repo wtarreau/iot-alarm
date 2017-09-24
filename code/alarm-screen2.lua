8bb8f4d41c7b18af3c97850307b1bb5a87c8c310
Willy Tarreau <w@1wt.eu>
2017-09-24 15:48:56 +0200

    code: alarm: allow to configure the alarm time with a long press on button 2
    Now each digit can be changed individually using button 1.

--- code/alarm-screen2.lua
+++ code/alarm-screen2.lua
@@ -1,30 +1,34 @@
 -- screen2 displays the alarm time
-local digit=0
+local digit=1
 
 function screen_show()
   disp_7seg_str(0,0,string.format("%02d:%02d",alarm_h(),alarm_m()),digit)
 end
 
 function screen_btn1_cb(btn,ev)
-  local ah,al=alarm_h(),alarm_m()
+  local ah,am=alarm_h(),alarm_m()
   if ev == 0 then return end
-  if ev == 4 then curr_screen=nil return end
+  if ev == 4 then screen_show() return end
 
-  if digit == 1 then       ah=ah+10
+  if ah < 0 or am < 0 then ah=0 am=0
+  elseif digit == 1 then   ah=ah+10
   elseif digit == 2 then   ah=ah+1
-  elseif digit == 4 then   al=al+10
-  elseif digit == 5 then   al=al+1
+  elseif digit == 4 then   am=am+10
+  elseif digit == 5 then   am=am+1
   end
 
-  if ah >= 24 then ah=-1 end
-  if am >= 60 then am=0 end
-  alarm=ah*60+am
+  if ah > 23 then ah=-1 end
+  if am > 59 then am=0 end
+  alarm=(ah >= 0) and ah*60+am or -1
 
-  if ev == 1 then curr_screen=nil end
+  if ev == 1 then screen_show() end
 end
 
 function screen_btn2_cb(btn,ev)
-  if ev == 0 then digit=(digit >= 5) and 0 or (digit==2) and 4 or (digit+1) end
+  if ev == 0 then
+    digit=(digit >= 5) and 1 or (digit==2) and 4 or (digit+1)
+    screen_show()
+  end
   if ev == 2 then want_screen=0 end
 end
 
